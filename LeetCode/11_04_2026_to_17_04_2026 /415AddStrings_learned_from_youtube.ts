80a596fceee0fdb07a0520fb7e1f2a8af40c5b2c
Aakash Thakur <aakashthakur20001972@gmail.com>
2026-04-15 23:37:44 +0530

    Add 415AddStrings learned from YouTube solution

---
+++ LeetCode/11_04_2026_to_17_04_2026 /415AddStrings_learned_from_youtube.ts
@@ -0,0 +1,22 @@
+function addStrings(num1: string, num2: string): string {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+
+    const result: string[] = [];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const digit1 = i >= 0 ? num1.charCodeAt(i) - 48 : 0;
+        const digit2 = j >= 0 ? num2.charCodeAt(j) - 48 : 0;
+
+        const sum = digit1 + digit2 + carry;
+
+        result.push(String(sum % 10));
+        carry = Math.floor(sum / 10);
+
+        i--;
+        j--;
+    }
+
+    return result.reverse().join("");
+}
